a79dad32186bd1a32f58afe1b8fb682747d016ae
rnovak <rnovak@pm-robertn-linux.ccbill-hq.local>
2015-09-28 11:17:42 -0700

    change

--- public-html/js/application.js
+++ public-html/js/application.js
@@ -3,7 +3,20 @@
     var application = angular.module("todo.app", ["ngRoute", "ngResource"]);
 
 
+    application.config(["$routeProvider", function($routeProvider) {
+         $routeProvider
+             .when('/', {
+                 'template': '<div>Hello World</div>'
+             })
+             .otherwise({
+                 redirectTo: '/'
+             })
+    }]);
 
+
+    var todoController = function($scope) {
+        
+    }
 
 
 })();
