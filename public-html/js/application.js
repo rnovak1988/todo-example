35192123b457c99bc69054be059b3ea2324c4e52
Robert Novak <rnovak17@gmail.com>
2015-10-05 16:20:31 -0700

    added icon for admin panel, now another thing I need to do :(

--- public-html/js/application.js
+++ public-html/js/application.js
@@ -1,13 +1,19 @@
 (function() {
 
-    var application = angular.module("todo.app", ["ngRoute", "ngResource"]);
+    var CONFIG = {
+        ROLES: {
+            USER: 'ROLE_USER',
+            ADMIN: 'ROLE_ADMIN'
+        }
+    };
 
+    var application = angular.module("todo.app", ["ngRoute", "ngResource"]);
 
     application.config(["$routeProvider", "$httpProvider", function($routeProvider, $httpProvider) {
 
          $routeProvider
              .when('/', {
-                 'template': '<div>{{tasks | json}}</div>',
+                 'template': '<div>{{tasks}}</div>',
                  'controller': 'taskController'
              })
              .otherwise({
@@ -16,10 +22,7 @@
 
     }]);
 
-
-    var taskController = function($scope, taskService) {
-        $scope.tasks = taskService.list();
-
+    var formController = function($scope, $rootScope, taskService) {
         $scope.emptyTask = function() {
             return {title: "", details: ""};
         };
@@ -28,10 +31,57 @@
 
         $scope.createNewTask = function() {
             taskService.create($scope.temp, function(data) {
-                console.log(data);
+                $scope.temp = $scope.emptyTask();
+                $rootScope.$emit('callCompleted', data);
             });
         };
+    };
 
+    var userService = function($resource) {
+        this.user = $resource('/user/', {}, {
+            current: {method: 'GET', url: '/user/current'},
+            currentRole: {method: 'GET', url: '/user/current/role'}
+        });
+    };
+
+    userService.prototype.getCurrentUser = function(callback) {
+        var user = this.user.current();
+
+        if (callback !== undefined && callback !== null && typeof callback === 'function') user.$promise.then(callback);
+
+        return user;
+    };
+
+    userService.prototype.getCurrentRole = function(callback) {
+        var role = this.user.currentRole();
+
+        if (callback !== undefined && callback !== null && typeof callback === 'function') role.$promise.then(callback);
+
+        return role;
+    };
+
+    var topController = function($scope, $rootScope, userService) {
+        $scope.currentUser = userService.getCurrentUser();
+        $scope.currentRole = userService.getCurrentRole();
+
+        $scope.is_admin = false;
+
+        $scope.currentRole.$promise.then(function(role) {
+            try {
+                $scope.is_admin = (role.role === CONFIG.ROLES.ADMIN);
+            } catch (fuckYou) {
+                $scope.is_admin = false;
+            }
+        });
+    };
+
+
+    var taskController = function($scope, $rootScope, taskService) {
+        $scope.tasks = taskService.list();
+
+        $rootScope.$on('callCompleted', function(event, args) {
+           $scope.tasks.push(args);
+        });
     };
 
     var taskService = function($resource) {
@@ -59,14 +109,16 @@
     };
 
     taskService.prototype.create = function(params, callback) {
-        console.log(params);
         var result = this.tasks.create(params);
         if (this.isFn(callback)) result.$promise.then(callback);
         return result;
     };
 
     application.service('taskService', ['$resource', taskService]);
-    application.controller('taskController', ['$scope', 'taskService', taskController]);
+    application.service('userService', ['$resource', userService]);
+    application.controller('taskController', ['$scope', '$rootScope', 'taskService', taskController]);
+    application.controller('formController', ['$scope', '$rootScope', 'taskService', formController]);
+    application.controller('topController', ['$scope', '$rootScope', 'userService', topController]);
 
 
 })();
